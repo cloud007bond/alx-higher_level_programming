1859d227e6084a3d14693c0fb3949ec24aaf4f27
Ezinwa charles <charleskelvin12@gmail.com>
2022-10-10 16:45:33 -0700

    errors and exception

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import dis
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -10,5 +9,4 @@ def magic_calculation(a, b):
         except:
             result = b + a
             break
-
     return result
