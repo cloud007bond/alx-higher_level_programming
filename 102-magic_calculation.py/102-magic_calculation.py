cfe6606f3d07182a4d14ef0576d7f2b7e0ac3aad
Ezinwa charles <charleskelvin12@gmail.com>
2022-10-10 17:23:52 -0700

    errors and exceptions

--- 102-magic_calculation.py/102-magic_calculation.py
+++ 102-magic_calculation.py/102-magic_calculation.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-
-
 def magic_calculation(a, b):
+
     result = 0
 
     for i in range(1, 3):
+
         try:
+
             if i > a:
+
                 raise Exception('Too far')
-
             result += a ** b / i
-        except:
-            result = b + a
-            break
 
+        except:
+
+            result = b + a
+
+            break
     return result
