d8fd97c75da8e25bf50113a1a7411016969de787
Ezinwa charles <charleskelvin12@gmail.com>
2022-10-25 09:54:26 -0700

    python in and out put

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,14 @@
-#!/usr/bin/python3
-""" Module for task 1 """
+
+dule for task 1 """
+
+
+
+
 
 def write_file(filename="", text=""):
-    """ function for writting files """
-    with open(filename, "w", enconding="utf-8") as f:
+
+    """ Function for writing files """
+
+    with open(filename, "w", encoding="utf-8") as f:
+
         return f.write(text)
