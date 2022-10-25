ea8e4dde5f74c9ab9609fc6ded9d4c074c319156
Ezinwa charles <charleskelvin12@gmail.com>
2022-10-25 10:05:45 -0700

    py input and output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-""" Module for task 2 """
+"""
+function that appends a string
+"""
+
 
 def append_write(filename="", text=""):
-    """ function for writing filrs """
-    with open(filename, "a", encoding="utf-8") as f:
+    """eturns the number of characters added:"""
+    with open(filename, 'a', encoding='utf=8') as f:
         return f.write(text)
