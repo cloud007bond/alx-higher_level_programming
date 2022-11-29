be7c530c6e6a334418026c418db0241125f1af8f
Ezinwa charles <charleskelvin12@gmail.com>
2022-11-29 09:08:18 -0800

    SQL_fixed

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,8 @@
--- Converts the entire database hbtn_0c_0 to UTF8.
-USE `hbtn_0c_0`
-ALTER TABLE `first_table`
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Lists all records with a `score` >= 10
+-- in the table `second_table` of the
+-- database `hbtn_0c_0` in MySQL Server.
+
+SELECT score, name
+FROM second_table
+WHERE `score` >= 10
+ORDER BY score DESC;
