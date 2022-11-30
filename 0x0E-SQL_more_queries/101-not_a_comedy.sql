a257abe0c868fef32bf2418d9e95547c412a878c
Ezinwa charles <charleskelvin12@gmail.com>
2022-11-30 09:41:43 -0800

    more_sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,22 +1,13 @@
--- Lists all genres not linked to
--- the show `Dexter`.
-
-SELECT DISTINCT tv_genres.name
-  FROM tv_shows
-       JOIN tv_show_genres
-       ON tv_shows.id = tv_show_genres.show_id
-
-       JOIN tv_genres
-       ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name NOT IN (
-    SELECT tv_genres.name
-    FROM tv_shows
-         JOIN tv_show_genres
-         ON tv_shows.id = tv_show_genres.show_id
-
-         JOIN tv_genres
-         ON tv_show_genres.genre_id = tv_genres.id
-    WHERE tv_shows.title = 'Dexter'
+-- Script that lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+SELECT	tv_shows.title
+FROM	tv_shows
+WHERE	tv_shows.id NOT IN
+(	SELECT	tv_shows.id
+	FROM	tv_shows
+	INNER JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+	INNER JOIN tv_genres
+	ON tv_show_genres.genre_id = tv_genres.id
+	WHERE	tv_genres.name = "Comedy"
 )
-ORDER BY tv_genres.name ASC;
-
+ORDER BY tv_shows.title;
--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,12 +1,9 @@
--- Lists all genres of the
--- Dexter` show.
-
-SELECT tv_genres.name
-  FROM tv_shows 
-       JOIN tv_show_genres
-       ON tv_shows.id = tv_show_genres.show_id
-
-       JOIN tv_genres
-       ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
+-- Script that lists all Comedy shows in the database hbtn_0d_tvshows
+SELECT	tv_shows.title
+FROM	tv_shows
+JOIN	tv_show_genres
+ON	tv_shows.id = tv_show_genres.show_id
+JOIN	tv_genres
+ON	tv_genres.id = tv_show_genres.genre_id
+WHERE	tv_genres.name = "Comedy"
+ORDER BY tv_shows.title ASC;
